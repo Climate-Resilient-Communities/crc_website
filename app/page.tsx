f6d86b1ecde4bed6dea06bdd916197a65881e12a
Pirashan Ravi <pirashanravi@Pirashans-MacBook-Air.local>
2024-02-19 02:13:45 -0500

    fixing mobile scroll

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Landing from '@/components/Landing'
 
+<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+
 export default function Home() {
   return (
     <div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,24 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-[#1C4405]  text-white py-12">
-      <div className="container mx-auto flex flex-wrap justify-between">
-        {/* Column 1: From our learning hub */}
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-6">
-          <h4 className="text-lg font-semibold mb-4">From our learning hub</h4>
-          <p className='text-xs'>Parks and Recreation</p>
-          <p className='text-xs'>Heat Islands</p>
-          <p className='text-xs'>Community Amenities</p>
-          <p className='text-xs'>Air and Noise Pollution</p>
-          <p className='text-xs'>Policy and Practice</p>
-        </div>
-
-        {/* Column 2: Sign up for our newsletter */}
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-6">
-          <h4 className="text-lg font-semibold mb-4">Sign up for our newsletter</h4>
-          {/* ... (rest of the content) */}
-        </div>
-
-        {/* Column 3: Social Media Links */}
+      <div className="container mx-auto flex flex-wrap justify-between px-6">
+        {/* Column 1: Social Media Links */}
         <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-6">
           <h4 className="text-lg font-semibold mb-4">Contact Us</h4>
           <p className='text-xs pb-2'>
@@ -35,6 +19,12 @@ const Footer = () => {
            <p className='text-xs'>
             Email: hello@crcgreen.com
            </p>
+        </div>
+
+            {/* Column 2: Sign up for our newsletter */}
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-6">
+          <h4 className="text-lg font-semibold mb-4">Sign up for our Newsletter</h4>
+
         </div>
       </div>
     </footer>
