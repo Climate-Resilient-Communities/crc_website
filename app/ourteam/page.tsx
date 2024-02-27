b297cf4d57c741ebfcf897cb2be50d9f6b6f84d9
Pirashan Ravi <pirashanravi@Pirashans-Air.hitronhub.home>
2024-02-27 01:59:59 -0500

    added ourteam page

--- app/ourteam/page.tsx
+++ app/ourteam/page.tsx
@@ -42,101 +42,35 @@ export default function OurTeam() {
                 </h3>
                 
                 <div className="items-center justify-center text-center">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div className="grid grid-cols-4 gap-4">
         {/* Row 1 */}
-        <div className="flex-1 p-4">
-          <h2 className="text-lg font-semibold">Project Management</h2>
-          <p>Coordinate project activites, manage timelines and ensure smooth execution of initiatives</p>
+        <div className="flex-1 col-span-1 p-4 min-w-8">
+        <img
+        src="/headshots/Helena.jpg"
+        alt="Helena Yu"
+        className="w-full h-auto rounded-lg"
+        style={{ minWidth: '8px' }}
+      />
         </div>
-        <div className="flex-1 p-4">
-          <h2 className="text-lg font-semibold">Data & Analytics</h2>
-          <p>Analyze climate resilience and climate justice data and 
-            generate insights to inform decision making and adaptation strategies </p>
-        </div>
-        {/* Row 2 */}
-        <div className="flex-1 p-4">
-          <h2 className="text-lg font-semibold">Software Development</h2>
-          <p>Maintain and enhance project website, and develop innovative 
-            solutions to improve availability, access and utilization of 
-            climate data and information</p>
-        </div>
-        <div className="flex-1 p-4">
-          <h2 className="text-lg font-semibold">Community & Partnership</h2>
-          <p>Build and nurture relationship switch community stakeholders 
-            and partners to enhance collaboration and engagement </p>
-        </div>
-        {/* Row 3 */}
-        <div className="flex-1 p-4">
-          <h2 className="text-lg font-semibold">Communications</h2>
-          <p>Craft compelling narratives, manage digital channels, 
-            and raise awareness about our projects and initiatives </p>
-        </div>
-        <div className="flex-1 p-4">
-          <h2 className="text-lg font-semibold">Funding</h2>
-          <p>Formulate and implement diversified and sustainable funding 
-            strategies, identify funding opportunities, write grant 
-            applications, and secure resources to support our programs</p>
+        <div className="flex-1 p-4 col-span-3">
+          <h2 className="text-lg font-bold text-left">Helena Yu</h2>
+          <h1 className="text-md font-semibold text-left pb-4">Project Maintainer, General & External</h1>
+          <p className="text-left text-xs md:text-base">Helena is a seasoned Data Scientist with experience across various 
+            industries, including technology, financial services, retail, and 
+            telecommunications. Notably, she led pro bono teams that conducted 
+            descriptive and natural language processing &#40;NLP&#41; analyses for Global 
+            Count, and created a predictive model to inform preventive 
+            interventions to end human trafficking in the U.S.. As an experienced 
+            leader with a background in Statistics, Computer Science, and 
+            Marketing, her prior experience as a Manager and Data Scientist at 
+            major organizations like Scotiabank, Accenture, Microsoft, and
+            Statistics Without Borders demonstrates her ability to manage 
+            complex projects and implement data-driven solutions. </p>
         </div>
       </div>
     </div>
-
-                <h4 className="justify-between pt-4 mx-auto font-bold text-center">
-                Government Agencies
-                </h4>
-                <p className="py-4">
-                Collaboration with Government Agencies is crucial 
-                for implementing last change, and CRC is committed 
-                to working hand-in-hand with agencies focused on 
-                urban development, climate action and community 
-                well-being. Through effective collaboration with 
-                government bodies, we can bridge the gap between 
-                top-down climate plans and grassroot actions as 
-                well as foster a more resilient and just society 
-                by supporting and strengthening the implementation 
-                of climate plans.
-                </p>
-                <h4 className="justify-between pt-4 mx-auto font-bold text-center">
-                    Academic Groups
-                </h4>
-                <p className="py-4">
-                The academic community plays a pivotal role in 
-                advancing knowledge and innovation. Through 
-                collaboration with academic institutions, CRC 
-                can ensure that our approach and tools are based 
-                on robust research and appropriate methodologies. 
-                By engaging with academic groups, we seek to foster 
-                a culture of continuous learning and innovation 
-                within communities, laying the foundation of 
-                sustainable resilience. 
-                </p>
-                <h4 className="justify-between pt-4 mx-auto font-bold text-center">
-                    Community Groups
-                </h4>
-                <p className="py-4 mb-12">
-                Central to the mission of Climate Resilient 
-                Communities is the collaboration with local 
-                Community Groups who serve as catalysts for 
-                change. Our project facilitates the building of 
-                community networks, connecting diverse groups 
-                facing similar climate vulnerabilities. Through 
-                peer learning and collaboration, communities 
-                can share insights, strategies, and experiences, 
-                enhancing collective resilience. Together with 
-                Community Groups, CRC is dedicated to creating 
-                a bottom-up approach that ensures the sustainability 
-                and effectiveness of climate resilience efforts.
-                </p>
-                </div>
-                <p className="font-bold text-sm mb-12">
-                Ready to partner with us or learn more about our 
-                initiatives? Email us at partnership@crcgreen.com 
-                to start a conversation today. You can also stay 
-                updated on our latest projects, events, and 
-                opportunities by signing up for our newsletter. 
-                Let's work together to build a greener, more 
-                resilient future for all.
-                </p>
-            </div>
+        </div>
+        </div>
         </div>
         <OurTeamFooter />
       </div>
--- components/Footers/OurTeamFooter.tsx
+++ components/Footers/OurTeamFooter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const OurTeamFooter = () => {
   return (
-    <footer className="bg-[#1C4405] text-white py-12">
+    <footer className="bg-[#b2770d] text-white py-12">
       <div className="container mx-auto flex flex-wrap justify-between px-6">
         {/* Column 1: Social Media Links */}
         <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-6">
