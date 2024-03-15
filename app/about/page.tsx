2d94146d8b5336e6ea8a3e2674be131d67109575
Pirashan Ravi <pirashanravi@Pirashans-Air.hitronhub.home>
2024-03-15 13:45:28 -0400

    fixed about landing page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ export default function About() {
         <div className="flex flex-col">
         <Header />
 
-        {/* Landing Image and Header */}
+        {/* Landing Image and Title*/}
         <div className="mx-auto mt-20 w-full" style={{ height: 'calc(100vh - 80px)' }}>
             <div className="absolute top-0 left-0 z-0 w-full h-full">
             <Image 
